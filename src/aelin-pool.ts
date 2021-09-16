3edd68a01f6af086256849df8d303027712291f9
alextheboredape <alextheboredape@protonmail.com>
2021-09-16 13:31:54 -0700

    final changes to the subgraph - will have to tests against the contracts and update it again as necessary per feedback from the audits

--- src/aelin-pool.ts
+++ src/aelin-pool.ts
@@ -3,30 +3,61 @@ import {
   PurchasePoolToken,
   WithdrawFromPool,
   AcceptDeal,
+  SetSponsor,
+  DealDetails,
+  Transfer,
 } from "./types/schema";
+import { Transfer as TransferEvent } from "./types/Transfer/Transfer";
+import { SetSponsor as SetSponsorEvent } from "./types/SetSponsor/SetSponsor";
 import { CreateDeal as CreateDealEvent } from "./types/CreateDeal/CreateDeal";
+import { DealDetails as DealDetailsEvent } from "./types/DealDetails/DealDetails";
 import { PurchasePoolToken as PurchasePoolTokenEvent } from "./types/PurchasePoolToken/PurchasePoolToken";
 import { WithdrawFromPool as WithdrawFromPoolEvent } from "./types/WithdrawFromPool/WithdrawFromPool";
 import { AcceptDeal as AcceptDealEvent } from "./types/AcceptDeal/AcceptDeal";
 import { AelinDeal } from "./types/templates";
 
+export function handleSetSponsor(event: SetSponsorEvent): void {
+  let setSponsorEntity = new SetSponsor(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  setSponsorEntity.sponsor = event.params.sponsor;
+  setSponsorEntity.save();
+}
+
+export function handlePoolTransfer(event: TransferEvent): void {
+  let tranferEntity = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  tranferEntity.from = event.params.from;
+  tranferEntity.to = event.params.to;
+  tranferEntity.value = event.params.value;
+  tranferEntity.save();
+}
+
 export function handleCreateDeal(event: CreateDealEvent): void {
   let dealCreatedEntity = new DealCreated(event.params.dealContract.toHex());
   dealCreatedEntity.name = event.params.name;
   dealCreatedEntity.symbol = event.params.symbol;
-  dealCreatedEntity.underlyingDealToken = event.params.underlyingDealToken;
-  dealCreatedEntity.dealPurchaseTokenTotal =
-    event.params.dealPurchaseTokenTotal;
-  dealCreatedEntity.underlyingDealTokenTotal =
-    event.params.underlyingDealTokenTotal;
-  dealCreatedEntity.vestingPeriod = event.params.vestingPeriod;
-  dealCreatedEntity.vestingCliff = event.params.vestingCliff;
-  dealCreatedEntity.redemptionPeriod = event.params.redemptionPeriod;
-  dealCreatedEntity.holder = event.params.holder;
-  dealCreatedEntity.poolTokenMaxPurchaseAmount =
-    event.params.poolTokenMaxPurchaseAmount;
-
+  dealCreatedEntity.sponsor = event.params.sponsor;
+  dealCreatedEntity.poolAddress = event.params.poolAddress;
   dealCreatedEntity.save();
+}
+
+export function handleDealDetails(event: DealDetailsEvent): void {
+  let dealDetailsEntity = new DealDetails(event.params.dealContract.toHex());
+  dealDetailsEntity.underlyingDealToken = event.params.underlyingDealToken;
+  dealDetailsEntity.purchaseTokenTotalForDeal =
+    event.params.purchaseTokenTotalForDeal;
+  dealDetailsEntity.underlyingDealTokenTotal =
+    event.params.underlyingDealTokenTotal;
+  dealDetailsEntity.vestingPeriod = event.params.vestingPeriod;
+  dealDetailsEntity.vestingCliff = event.params.vestingCliff;
+  dealDetailsEntity.proRataRedemptionPeriod =
+    event.params.proRataRedemptionPeriod;
+  dealDetailsEntity.openRedemptionPeriod = event.params.openRedemptionPeriod;
+  dealDetailsEntity.holder = event.params.holder;
+
+  dealDetailsEntity.save();
 
   // use templates to create a new deal to track events
   AelinDeal.create(event.params.dealContract);
@@ -67,6 +98,9 @@ export function handleAcceptDeal(event: AcceptDealEvent) {
   acceptDealEntity.poolAddress = event.params.poolAddress;
   acceptDealEntity.dealAddress = event.params.dealAddress;
   acceptDealEntity.poolTokenAmount = event.params.poolTokenAmount;
+  acceptDealEntity.underlyingToHolderAmt = event.params.underlyingToHolderAmt;
+  acceptDealEntity.aelinFee = event.params.aelinFee;
+  acceptDealEntity.sponsorFee = event.params.sponsorFee;
 
   acceptDealEntity.save();
 }
