96f0acba2b463267d14a15bf7a9989407f273742
Clement Balestrat <clement.balestrat@gmail.com>
2021-11-03 16:50:23 +0100

    add pool timestamp

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -113,6 +113,15 @@ export class PoolCreated extends Entity {
   set purchaseExpiry(value: BigInt) {
     this.set("purchaseExpiry", Value.fromBigInt(value));
   }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
 }
 
 export class PurchasePoolToken extends Entity {
