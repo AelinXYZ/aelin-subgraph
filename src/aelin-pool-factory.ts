ce778cb7e710bdafd3f5053ccd6c8f6c989c0e02
alextheboredape <alextheboredape@protonmail.com>
2021-11-04 15:07:06 -0400

    add total pools created for the pagination section in all pools - in the future can break this into active and inactive pools as well

--- src/aelin-pool-factory.ts
+++ src/aelin-pool-factory.ts
@@ -1,8 +1,18 @@
-import { PoolCreated } from "./types/schema";
+import { PoolCreated, TotalPoolsCreated } from "./types/schema";
 import { CreatePool as CreatePoolEvent } from "./types/AelinPoolFactory/AelinPoolFactory";
 import { AelinPool } from "./types/templates";
+import { ONE } from "./helpers";
 
 export function handleCreatePool(event: CreatePoolEvent): void {
+  let totalPoolsCreatedEntity = TotalPoolsCreated.load("1");
+  if (totalPoolsCreatedEntity == null) {
+    totalPoolsCreatedEntity = new TotalPoolsCreated("1");
+    totalPoolsCreatedEntity.count = ONE;
+  } else {
+    totalPoolsCreatedEntity.count = totalPoolsCreatedEntity.count.plus(ONE);
+  }
+  totalPoolsCreatedEntity.save();
+
   let poolCreatedEntity = new PoolCreated(event.params.poolAddress.toHex());
   poolCreatedEntity.name = event.params.name;
   poolCreatedEntity.symbol = event.params.symbol;
