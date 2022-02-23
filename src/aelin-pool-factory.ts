6b2b1d93acf11a8e452ec078d4899f787fd37978
Linus <linuscrypto@protonmail.com>
2022-02-23 23:03:01 +0100

    subgraph changes added

--- src/aelin-pool-factory.ts
+++ src/aelin-pool-factory.ts
@@ -1,43 +1,49 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { PoolCreated, TotalPoolsCreated, AelinToken } from "./types/schema";
-import { CreatePool as CreatePoolEvent } from "./types/AelinPoolFactory/AelinPoolFactory";
-import { AelinPool } from "./types/templates";
-import { ONE } from "./helpers";
-import { PoolStatus } from "./enum";
+import { BigInt } from '@graphprotocol/graph-ts';
+import { PoolCreated, TotalPoolsCreated, AelinToken } from './types/schema';
+import { CreatePool as CreatePoolEvent } from './types/AelinPoolFactory/AelinPoolFactory';
+import { AelinPool } from './types/templates';
+import { ERC20 } from './types/AelinPoolFactory/ERC20';
+import { ONE } from './helpers';
+import { PoolStatus } from './enum';
 
 export function handleCreatePool(event: CreatePoolEvent): void {
-  let totalPoolsCreatedEntity = TotalPoolsCreated.load("1");
-  if (totalPoolsCreatedEntity == null) {
-    totalPoolsCreatedEntity = new TotalPoolsCreated("1");
-    totalPoolsCreatedEntity.count = ONE;
-  } else {
-    totalPoolsCreatedEntity.count = totalPoolsCreatedEntity.count.plus(ONE);
-  }
-  totalPoolsCreatedEntity.save();
+	let totalPoolsCreatedEntity = TotalPoolsCreated.load('1');
+	if (totalPoolsCreatedEntity == null) {
+		totalPoolsCreatedEntity = new TotalPoolsCreated('1');
+		totalPoolsCreatedEntity.count = ONE;
+	} else {
+		totalPoolsCreatedEntity.count = totalPoolsCreatedEntity.count.plus(ONE);
+	}
+	totalPoolsCreatedEntity.save();
 
-  let poolCreatedEntity = new PoolCreated(event.params.poolAddress.toHex());
-  poolCreatedEntity.name = event.params.name;
-  poolCreatedEntity.symbol = event.params.symbol;
-  poolCreatedEntity.purchaseTokenCap = event.params.purchaseTokenCap;
-  poolCreatedEntity.purchaseToken = event.params.purchaseToken;
-  poolCreatedEntity.duration = event.params.duration;
-  poolCreatedEntity.sponsorFee = event.params.sponsorFee;
-  poolCreatedEntity.sponsor = event.params.sponsor;
-  poolCreatedEntity.purchaseDuration = event.params.purchaseDuration;
-  poolCreatedEntity.purchaseExpiry = event.params.purchaseDuration.plus(
-    event.block.timestamp
-  );
-  poolCreatedEntity.timestamp = event.block.timestamp;
-  let aelinPoolTokenEntity = AelinToken.load(event.params.poolAddress.toHex());
-  if (aelinPoolTokenEntity != null) {
-    poolCreatedEntity.purchaseTokenDecimals = aelinPoolTokenEntity.decimals;
-  }
-  poolCreatedEntity.hasAllowList = event.params.hasAllowList;
-  poolCreatedEntity.poolStatus = PoolStatus.PoolOpen;
-  poolCreatedEntity.contributions = BigInt.fromI32(0);
+	let poolCreatedEntity = new PoolCreated(event.params.poolAddress.toHex());
+	poolCreatedEntity.name = event.params.name;
+	poolCreatedEntity.symbol = event.params.symbol;
+	poolCreatedEntity.purchaseTokenCap = event.params.purchaseTokenCap;
+	poolCreatedEntity.purchaseToken = event.params.purchaseToken;
+	poolCreatedEntity.duration = event.params.duration;
+	poolCreatedEntity.sponsorFee = event.params.sponsorFee;
+	poolCreatedEntity.sponsor = event.params.sponsor;
+	poolCreatedEntity.purchaseDuration = event.params.purchaseDuration;
+	poolCreatedEntity.purchaseExpiry = event.params.purchaseDuration.plus(
+		event.block.timestamp
+	);
+	poolCreatedEntity.timestamp = event.block.timestamp;
+	let aelinPoolTokenEntity = AelinToken.load(event.params.poolAddress.toHex());
+	if (aelinPoolTokenEntity != null) {
+		poolCreatedEntity.purchaseTokenDecimals = aelinPoolTokenEntity.decimals;
+	}
+	poolCreatedEntity.hasAllowList = event.params.hasAllowList;
+	poolCreatedEntity.poolStatus = PoolStatus.PoolOpen;
+	poolCreatedEntity.contributions = BigInt.fromI32(0);
 
-  poolCreatedEntity.save();
+	//get purchaseToken symbol and decimals
+	const purchaseToken = ERC20.bind(event.params.purchaseToken);
+	poolCreatedEntity.purchaseTokenSymbol = purchaseToken.symbol();
+	// poolCreatedEntity.purchaseTokenDecimals = purchaseToken.decimals();
 
-  // use templates to create a new pool to track events
-  AelinPool.create(event.params.poolAddress);
+	poolCreatedEntity.save();
+
+	// use templates to create a new pool to track events
+	AelinPool.create(event.params.poolAddress);
 }
