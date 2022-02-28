e97b707019913e9eca02fff2985082ca95b81310
Aelin-xyz <89638770+Aelin-xyz@users.noreply.github.com>
2022-03-01 06:36:31 +1100

    add total supply and change subgraph names (#30)
    Co-authored-by: alextheboredape <alextheboredape@protonmail.com>

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, Address } from "@graphprotocol/graph-ts";
 
 export let ONE = BigInt.fromI32(1);
+
+export let ZERO_ADDRESS = Address.fromHexString("0x0000000000000000000000000000000000000000");
