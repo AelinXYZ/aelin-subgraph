9cd6ba55381a17b0c39cb21a875a27cb1e2277bc
Linus <linuscrypto@protonmail.com>
2022-08-25 22:31:31 +0200

    Added husky + eslint + prettier

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,34 +1,33 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { ERC20 } from "../types/templates/AelinPool/ERC20"
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { ERC20 } from '../types/templates/AelinPool/ERC20'
 
 export function getTokenSymbol(address: Address): string {
-    const token = ERC20.bind(address)
-    const symbol = token.try_symbol()
-    if(symbol.reverted) {
-        return ""
-    }
+  const token = ERC20.bind(address)
+  const symbol = token.try_symbol()
+  if (symbol.reverted) {
+    return ''
+  }
 
-    return symbol.value
+  return symbol.value
 }
 
 // @ts-ignore
 export function getTokenDecimals(address: Address): i32 {
-    const token = ERC20.bind(address)
-    const decimals = token.try_decimals()
-    if(decimals.reverted) {
-        return 0
-    }
+  const token = ERC20.bind(address)
+  const decimals = token.try_decimals()
+  if (decimals.reverted) {
+    return 0
+  }
 
-    return decimals.value
+  return decimals.value
 }
 
 export function getTokenTotalSupply(address: Address): BigInt {
-    const token = ERC20.bind(address)
-    const totalSupply = token.try_totalSupply()
-    if(totalSupply.reverted) {
-        return BigInt.fromI32(0)
-    }
+  const token = ERC20.bind(address)
+  const totalSupply = token.try_totalSupply()
+  if (totalSupply.reverted) {
+    return BigInt.fromI32(0)
+  }
 
-    return totalSupply.value
+  return totalSupply.value
 }
-
