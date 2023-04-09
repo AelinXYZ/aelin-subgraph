6c83ec417d5acebc0bda19473c6d85d7c1fa857b
0xDmitry <sign-69@mail.ru>
2023-04-09 11:53:06 -0300

    Also ERC1155

--- src/services/entities.ts
+++ src/services/entities.ts
@@ -323,7 +323,7 @@ function createNft1155CollectionRules<T>(event: T): void {
     poolCreatedEntity.hasNftList = true
 
     let nftCollectionRules = poolCreatedEntity.nftCollectionRules
-    if (nftCollectionRules !== null) {
+    if (nftCollectionRules != null) {
       nftCollectionRules.push(nftCollectionRule.id)
       poolCreatedEntity.nftCollectionRules = nftCollectionRules
     }
